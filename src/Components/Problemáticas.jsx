abff83b8fe753463bcfc16db8e75c3139d484719
Facundo <facundojsole@gmail.com>
2022-11-16 20:47:00 -0300

    Agregué icon cards + css, puse iconos en assets

--- src/Components/Problemáticas.jsx
+++ src/Components/Problemáticas.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import IconCard from "./IconCard";
+import image from '../Assets/tear.svg'
+import image2 from '../Assets/estate.svg'
+import image3 from '../Assets/users-alt.svg'
+import '../CSS/Problematicas.css'
 
 export default function Problematicas() {
   return (
-    <div>
-      <h2>Trabajamos en busca de la equidad social </h2>
-      <p>
+    <div className="problematica-container">
+      <h2 className="titulo">Trabajamos en busca de la <span className="enfasis">equidad social</span> </h2>
+      <p className="problematica-text">
         Abordamos temáticas en relación con la alimentación, salud,
         infraestructura, acceso al agua potable y perspectiva de género.
       </p>
-      <div>
-        <div>Agua potable</div>
-        <div>infraestructura</div>
-        <div>Perspectiva de género</div>
+      <div className="icon-container">
+        <IconCard img={image} title='Agua potable' />
+        <IconCard img={image2} title='Infraestructura' />
+        <IconCard img={image3} title='Perspectiva de género' />
       </div>
     </div>
   );
