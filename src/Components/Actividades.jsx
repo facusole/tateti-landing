3a72a335ab12fe24af0e2492bd72e56b9f729d27
PatricioKach <kachonosky@gmail.com>
2022-11-27 17:25:01 -0300

    "componente Proyectos"

--- src/Components/Actividades.jsx
+++ src/Components/Actividades.jsx
@@ -5,58 +5,34 @@ import image from "../Assets/users-alt.svg";
 
 export default function Actividades() {
   return (
-<<<<<<< HEAD
-    <div className="container-actividades">
+    <div className="container-actividades mg-inline">
       <h2 className="section-title">
         Construimos un vínculo con{" "}
         <span className="enfasis">las comunidades</span>{" "}
       </h2>
+
       <div className="container-cards-act">
-        <div className="card-actividades">
-          <IconCard
-            img={image}
-            title="Envio de encomiendas"
-            text="Contienen alimentos, elementos de higiene y salud, útiles escolares,
+        <IconCard
+          img={image}
+          title="Envio de encomiendas"
+          text="Contienen alimentos, elementos de higiene y salud, útiles escolares,
             entre otros."
-          />
-        </div>
+        />
 
-        <div>
-          <IconCard
-            img={image}
-            title="Viajes"
-            text="Visitamos las escuelas para conocer el territorio y entender su
+        <IconCard
+          img={image}
+          title="Viajes"
+          text="Visitamos las escuelas para conocer el territorio y entender su
             realidad social."
-          />
-        </div>
+        />
 
-        <div>
-          <IconCard
-            img={image}
-            title="Actividades lúdicas"
-            text="Promovemos las actividades recreativas y artísticas desde un enfoque
+        <IconCard
+          img={image}
+          title="Actividades lúdicas"
+          text="Promovemos las actividades recreativas y artísticas desde un enfoque
             basado en el juego."
-          />
-        </div>
-=======
-    <div className="container-actividades mg-inline">
-
-      <h2 className="section-title">Construimos un vínculo con <span className="enfasis">las comunidades</span> </h2>
-
-      <div className="container-cards-act">
-
-          <IconCard img={image} title='Envio de encomiendas' text='Contienen alimentos, elementos de higiene y salud, útiles escolares,
-            entre otros.' />
-
-          <IconCard img={image} title='Viajes' text='Visitamos las escuelas para conocer el territorio y entender su
-            realidad social.' />
-
-          <IconCard img={image} title='Actividades lúdicas' text='Promovemos las actividades recreativas y artísticas desde un enfoque
-            basado en el juego.' />
-            
->>>>>>> ed19a1db05959fd670deb1edc24a0307ce53850b
+        />
       </div>
-
     </div>
   );
 }
--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,18 +1,32 @@
+import { Drop } from "iconsax-react";
 import React from "react";
 import "../CSS/Proyectos.css";
 
 export default function Proyectos() {
   return (
-    <div>
-      <h2>Nuestras escuelas</h2>
-      <p>Proporcionamos condiciones que faciliten el acceso a la educación. </p>
-
-      <h2>Escuela N° 806 Chaco</h2>
-      <p>
-        En el 2017 pintamos toda la escuela y conseguimos un filtro de agua
-        potable junto a Proyecto Agua Segura. En 2018 llevamos un taller de ESI
-        con el objetivo de promover la salud.{" "}
-      </p>
+    <div className="mg-inline ">
+      <div className="first-text">
+        {" "}
+        <h2>
+          <Drop />
+          Nuestras escuelas
+        </h2>
+        <p>
+          Proporcionamos condiciones que faciliten el acceso a la educación.{" "}
+        </p>
+      </div>
+      <div className="text-proyectos">
+        <h2>Escuela N° 806 Chaco</h2>
+        <p>
+          En el 2017 pintamos toda la escuela y conseguimos un filtro de agua
+          potable junto a Proyecto Agua Segura.
+        </p>
+        <p>
+          {" "}
+          En 2018 llevamos un taller de ESI con el objetivo de promover la
+          salud.{" "}
+        </p>
+      </div>
 
       <div className="img-container1">
         <img src="image.png" alt="imgTateti" className="imgTateti1" />
@@ -29,12 +43,14 @@ export default function Proyectos() {
           <p className="title-img">Filtro de agua </p>
         </div>
       </div>
-      <h2>Escuela N° 6195 Santa Fe</h2>
-      <p>
-        En 2022 dictamos un ciclo de talleres de ESI junto a Preservate,
-        abordando temáticas de vínculos saludables, estereotipos de género e
-        higiene menstrual.{" "}
-      </p>
+      <div className="text-proyectos">
+        <h2>Escuela N° 6195 Santa Fe</h2>
+        <p>
+          En 2022 dictamos un ciclo de talleres de ESI junto a Preservate,
+          abordando temáticas de vínculos saludables, estereotipos de género e
+          higiene menstrual.{" "}
+        </p>
+      </div>
       <div className="img-container1">
         <img src="image.png" alt="imgTateti" className="imgTateti1" />
         <div className="name-img1">
